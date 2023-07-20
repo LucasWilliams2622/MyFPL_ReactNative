9a3ecb4d266b9a6fb97e6c1c2c92d5695ee011e0
Lucas_Williams <101234675+LucasWilliams2622@users.noreply.github.com>
2023-07-20 22:26:24 +0700

    smt new

---
+++ src/components/ItemNews.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const ItemNews = () => {
+  return (
+    <View>
+      <Text>ItemNews</Text>
+    </View>
+  )
+}
+
+export default ItemNews
+
+const styles = StyleSheet.create({})
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -2,11 +2,13 @@ import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { AppStyle } from '../constants/AppStyle'
 import AppHeader from '../components/AppHeader'
+import ItemNews from '../components/ItemNews'
 
 const Home = () => {
   return (
     <SafeAreaView style={AppStyle.container}>
       <AppHeader />
+      <ItemNews/>
     </SafeAreaView>
   )
 }
