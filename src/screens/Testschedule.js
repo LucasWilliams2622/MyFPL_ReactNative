6764ad43536b5f3b007e71c8b2109c6cac946248
trung-hieu-vscode <94751659+trung-hieu-vscode@users.noreply.github.com>
2023-07-20 22:47:31 +0700

    up

---
+++ src/screens/Testschedule.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Textscheule = () => {
+  return (
+    <View>
+      <Text>Textscheule</Text>
+    </View>
+  )
+}
+
+export default Textscheule
+
+const styles = StyleSheet.create({})
